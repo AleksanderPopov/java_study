ad925e30ba93cd7909dc7c8b6b41b73357590509
Alexander Popov <aleksander.popov.93@gmail.com>
2017-04-01 21:39:15 +0300

    master - add a few classes for javaonlinestudy

---
+++ src/main/java/com/alexstudy/courses/javaonlinestudy/advanced/generics/Product.java
@@ -0,0 +1,14 @@
+package com.alexstudy.courses.javaonlinestudy.advanced.generics;
+
+/**
+ * Created by Alex on 4/1/2017.
+ */
+public class Product<T extends Product> implements Comparable<T> {
+    String name;
+    int price;
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
+    }
+}
