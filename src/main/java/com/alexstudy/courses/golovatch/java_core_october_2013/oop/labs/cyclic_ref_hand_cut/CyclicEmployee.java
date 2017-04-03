083ee197e857668ed25baa09e6ccd1cc8ecde0d4
Alexander Popov <apop@ciklum.com>
2017-04-03 16:04:21 +0300

    master - oop lab in progress

---
+++ src/main/java/com/alexstudy/courses/golovatch/java_core_october_2013/oop/labs/cyclic_ref_hand_cut/CyclicEmployee.java
@@ -0,0 +1,102 @@
+package com.alexstudy.courses.golovatch.java_core_october_2013.oop.labs.cyclic_ref_hand_cut;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by apop on 4/3/2017.
+ */
+public class CyclicEmployee {
+    private int age;
+    private String name;
+    private CyclicEmployee boss;
+    private List<CyclicEmployee> subordinate;
+
+    public CyclicEmployee(int age, String name, CyclicEmployee boss, List<CyclicEmployee> subordinate) {
+        this.age = age;
+        this.name = name;
+        this.boss = boss;
+        this.subordinate = subordinate;
+    }
+
+    public CyclicEmployee(int age, String name, CyclicEmployee boss, CyclicEmployee... subordinate) {
+        this(age, name, boss, Stream.of(subordinate).collect(toList()));
+    }
+
+    public CyclicEmployee(int age, String name, CyclicEmployee boss) {
+        this(age, name, boss, new ArrayList<>());
+    }
+
+    public CyclicEmployee(CyclicEmployee cyclicEmployee) {
+        this.age = cyclicEmployee.age;
+        this.name = cyclicEmployee.name;
+        this.boss = cyclicEmployee.boss;
+        this.subordinate = cyclicEmployee.subordinate;
+    }
+
+    static CyclicEmployee alex() {
+        CyclicEmployee alex = new CyclicEmployee(24, "Alex", new CyclicEmployee(32, "An", null));
+        CyclicEmployee gramatik = new CyclicEmployee(24, "Gramatik", new CyclicEmployee(32, "An", null));
+        alex.subordinate.add(gramatik);
+        gramatik.subordinate.add(alex);
+        return alex;
+    }
+
+    @Override
+    public String toString() {
+        List<CyclicEmployee> employees = new ArrayList<>();
+        for (CyclicEmployee employee : subordinate) {
+            if (this.equals(employee)) employees.add(new CyclicEmployeeSimple(employee));
+            else employees.add(employee);
+        }
+
+        return String.format("Employee[age='%d', name='%s', boss='%s', subordinate='%s']",
+                age,
+                name,
+                boss == null ? "null" : boss.toString(),
+                subordinate == null
+                        ? "null"
+                        : subordinate.stream().map(elem -> elem.equals(this) ? new CyclicEmployeeSimple(elem).toString() : elem.toString()).collect(toList()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        CyclicEmployee that = (CyclicEmployee) o;
+
+        if (age != that.age) return false;
+        if (!name.equals(that.name)) return false;
+        if (!boss.equals(that.boss)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + boss.hashCode();
+        result = 31 * result + (subordinate != null ? subordinate.hashCode() : 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CyclicEmployee.alex().toString());
+    }
+
+    class CyclicEmployeeSimple extends CyclicEmployee {
+        CyclicEmployeeSimple(CyclicEmployee employee) {
+            super(employee);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Employee[age='%d', name='%s', boss='%s']",
+                    super.age, super.name, super.boss == null ? "no" : super.boss.toString());
+        }
+    }
+}
---
+++ src/main/java/com/alexstudy/courses/golovatch/java_core_october_2013/oop/labs/cyclic_ref_jakarta/Person.java
@@ -0,0 +1,24 @@
+package com.alexstudy.courses.golovatch.java_core_october_2013.oop.labs.cyclic_ref_jakarta;
+
+/**
+ * Created by apop on 4/3/2017.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean smoker;
+
+//    public String toString() {
+//        return new ToStringBuilder(this).
+//                append("name", name).
+//                append("age", age).
+//                append("smoker", smoker).
+//                toString();
+//    }
+//
+//    private class ToStringBuilder {
+//        public ToStringBuilder(Person person) {
+//            person.getClass()
+//        }
+//    }
+}
